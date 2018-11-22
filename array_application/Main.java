dc42cb81af4cd3044971dd905987931953abea9f
king_zl <15910344021@163.com>
2018-11-22 15:53:36 +0800

    add

--- array_application/Main.java
+++ array_application/Main.java
@@ -2,11 +2,10 @@ package array_application;
 
 public class Main {
     public static void main(String[] args){
-//        Solution1 s1 = new Solution1();
-//        String s2 = "3424251233254345343543543";
-//        String s3 = "123";
-//        String res = s1.calculateBigInteger(s2, s3);
-//        System.out.println(res);
-        System.out.println('1'+'2'-'0'-'0');
+        Solution1 s1 = new Solution1();
+        String s2 = "3424251233254345343543543";
+        String s3 = "123";
+        String res = s1.calculateBigInteger(s2, s3);
+        System.out.println(res);
     }
 }
